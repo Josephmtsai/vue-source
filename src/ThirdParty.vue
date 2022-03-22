f65f79c8582b86686da1c5682b785a6927690af1
joseph <mmx362003@gmail.com>
2022-03-22 23:37:35 +0800

    fix bundle name

---
+++ src/ThirdParty.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+</script>
+
+<template>
+  <HeaderComponent></HeaderComponent>
+  <teleport to="#footer-div">
+    <FooterComponent></FooterComponent>
+  </teleport>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/thirdParty.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import ThirdParty from './ThirdParty.vue';
+import './index.css';
+createApp(ThirdParty).mount('#header-component');
