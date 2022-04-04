0371e4e4fd9b1da9af152a25b8a7218ee7e22ccd
joseph <mmx362003@gmail.com>
2022-04-04 23:56:25 +0800

    test variable

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,31 +4,59 @@
       <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
     </div>
     <div class="block lg:hidden">
-      <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-      </button>
+      <button
+        class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+      ></button>
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div class="text-sm lg:flex-grow">
-        <a href="https://flowbite.com" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"> Docs </a>
-        <a href="https://flowbite.com" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"> Examples </a>
-        <a href="https://flowbite.com" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"> Blog </a>
+        <a
+          href="https://flowbite.com"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Docs
+        </a>
+        <a
+          href="https://flowbite.com"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Examples
+        </a>
+        <a
+          href="https://flowbite.com"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+        >
+          Blog
+        </a>
       </div>
-      <div>
-        {{ url }}
-      </div>
+      <div>uv {{ outSideVariable }}</div>
+      <div>uv data {{ outSideNumber }}</div>
     </div>
   </nav>
 </template>
 <script>
+import { ref, watch, watchEffect } from 'vue';
 export default {
-  created() {
-    this.url = location.href;
-  },
-  data() {
+  name: 'HelloWorld',
+  setup() {
+    const outSideNumber = ref(uv ? uv.data : 0);
+    const outSideVariable = ref(uv);
+    const url = ref(location.href);
+    watch(outSideVariable, (newValue, oldValue) => {
+      console.log('watch outSideVariable', newValue, oldValue);
+    });
+
+    watch(outSideVariable, (newValue, oldValue) => {
+      console.log('watch outSideNumber', newValue, oldValue);
+    });
+    watchEffect(() => {
+      console.log('watchEffect', outSideVariable.value, outSideNumber.value);
+    });
     return {
-      url: '',
+      outSideNumber,
+      url,
+      outSideVariable,
     };
   },
-  name: 'HeaderComponent',
 };
 </script>
