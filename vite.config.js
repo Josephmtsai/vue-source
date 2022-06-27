10d5eb075c209919476d5d830d08fe7f520830ef
joseph <mmx362003@gmail.com>
2022-06-27 23:54:38 +0800

    add test lib

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     rollupOptions: {
+      input: {
+        main: './src/main.js',
+        sample: path.resolve('./src/lib/sample.js'),
+      },
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
