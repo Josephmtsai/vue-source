5d9bd9fba6dbb3d9a1b7f361f276adf42909fd50
joseph <mmx362003@gmail.com>
2022-06-28 00:07:24 +0800

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { fileURLToPath } from 'url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     rollupOptions: {
       input: {
-        main: './src/main.js',
+        main: path.resolve(__dirname, 'index.html'),
         sample: path.resolve('./src/lib/sample.js'),
       },
       output: {
